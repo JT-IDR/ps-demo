4c95e63bc662c0fd728ce4acc725aefefea7d91a
Jacques Terblanche <Jacques.Terblanche@idrgroup.com>
2023-01-16 12:52:43 +0200

    CSS validation - to be fixed because validation does not work for every combo

--- src/app/user-settings-from/user-settings-from.component.ts
+++ src/app/user-settings-from/user-settings-from.component.ts
@@ -1,19 +1,29 @@
 import {Component} from '@angular/core';
 import {UserSettings} from "../data/user-settings.model";
+import {NgForm, NgModel} from "@angular/forms";
 
 @Component({
   selector: 'app-user-settings-from',
   templateUrl: './user-settings-from.component.html',
   styleUrls: ['./user-settings-from.component.css'],
-  // exportAs: 'userForm'
 })
 export class UserSettingsFromComponent {
+  isInvalid: boolean | null = false;
   originalUserSettings: UserSettings = {
-    name: 'Minalan Spellmonger',
-    emailOffers: false,
-    interfaceStyle: 'dark',
-    subscriptionType: 'monthly',
-    notes: `He's the best wizard in the world!`
+    name: null,
+    emailOffers: null,
+    interfaceStyle: null,
+    subscriptionType: null,
+    notes: null
   }
-  userSettings: UserSettings = {...this.originalUserSettings }
+  userSettings: UserSettings = {...this.originalUserSettings}
+
+  onSubmit(form: NgForm) {
+
+    console.log('in onSubmit ', form.valid);
+  }
+
+  onBlur( form: NgForm, nameField: NgModel) {
+    return this.isInvalid = !form.submitted || nameField.valid;
+  }
 }
